62c7ac0ff71a3d50bed6991cd7ce5e435cac3d41
Lukas Reeves <lsreeves.921@gmail.com>
2022-01-21 15:56:38 -0500

    menu scripts

---
+++ NoNameGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+
+
+//Created by Lukas Reeves//
+
+{
+
+    Scene currentScene;
+    GameObject[] pauseMenu;
+    GameObject[] settingsMenu;
+    GameObject[] playerUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseMenu = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        settingsMenu = GameObject.FindGameObjectsWithTag("ShowOnSettings");
+        playerUI = GameObject.FindGameObjectsWithTag("PlayerUI");
+        HidePaused();
+        HideSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            currentScene = SceneManager.GetActiveScene();
+
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                ShowPaused();
+                Debug.Log("Showing menu");
+                Debug.Log(pauseMenu[0]);
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                HidePaused();
+                HideSettings();
+            }
+        }
+    }
+
+    // Reloads current scene
+    public void Reload()
+    {
+        SceneManager.LoadScene(currentScene.name);
+    }
+
+    // Controls pause
+    public void PauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            ShowPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            HidePaused();
+        }
+    }
+
+    // Shows all objects with ShowOnPause tag
+    public void ShowPaused()
+    {
+        foreach (GameObject g in pauseMenu)
+        {
+            g.SetActive(true);
+        }
+        foreach (GameObject g in playerUI)
+        {
+            g.SetActive(false);
+        }
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    // Hides all objects with ShowOnPause tag
+    public void HidePaused()
+    {
+        foreach (GameObject g in pauseMenu)
+        {
+            g.SetActive(false);
+        }
+        foreach (GameObject g in playerUI)
+        {
+            g.SetActive(true);
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Shows all objects with ShowOnSettings tag
+    public void ShowSettings()
+    {
+        foreach (GameObject g in settingsMenu)
+        {
+            g.SetActive(true);
+        }
+        foreach (GameObject g in playerUI)
+        {
+            g.SetActive(false);
+        }
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    // Hides all objects with ShowOnSettings tag
+    public void HideSettings()
+    {
+        foreach (GameObject g in settingsMenu)
+        {
+            g.SetActive(false);
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Loads inputted scene
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ NoNameGame/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SettingsMenu : MonoBehaviour
+
+
+//Created by Lukas Reeves//
+
+{
+
+    public AudioMixer mainMixer;
+
+    public Dropdown resolutionDropdown;
+
+    public Toggle fullscreen;
+
+    public Slider volumeSlider;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        mainMixer.GetFloat("MasterVolume", out float value);
+        value = value / 20;
+        volumeSlider.value = (Mathf.Pow(10f, value));
+        fullscreen.isOn = Screen.fullScreen;
+
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+}
