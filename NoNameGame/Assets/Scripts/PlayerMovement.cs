82734361635e18ed89a9b032c94b7343a9afb25e
Lukas Reeves <lsreeves.921@gmail.com>
2022-01-21 15:52:58 -0500

    movement scripts & player prefab

---
+++ NoNameGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private AudioSource audioSource;
+    public AudioClip walking;
+    public AudioClip running;
+
+    public float walkSpeed;
+    public float runSpeed;
+
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 3f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    public bool isRunning;
+    bool isWalking;
+
+    //CharacterController charactercollider;//
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        audioSource = GetComponent<AudioSource>();
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (isRunning == false && move != Vector3.zero)
+        {
+            isWalking = true;
+            controller.Move(move * walkSpeed * Time.deltaTime);
+
+            //if (audioSource.isPlaying == false)
+            //{
+            //    audioSource.clip = walking;
+            //    audioSource.Play();
+            //}
+        }
+        else if (move == Vector3.zero || isRunning == true)
+        {
+            isWalking = false;
+
+            //if (audioSource.isPlaying == true && isRunning == false)
+            //{
+            //    audioSource.Pause();
+            //}
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) == false && move != Vector3.zero)
+        {
+
+            //if (audioSource.isPlaying == false || audioSource.clip != running)
+            //{
+            //    audioSource.clip = running;
+            //    audioSource.Play();
+            //}
+
+        }
+        else if (move == Vector3.zero || isWalking == false)
+        {
+            isRunning = false;
+            //if (audioSource.isPlaying == true && isWalking == false)
+            //{
+            //    audioSource.Pause();
+            //}
+        }
+
+        //if (Input.GetButtonDown("Jump") && isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        //}   
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
