19b041082e4652a3743560e48dee2e1f8829f9d9
Lukas Reeves <lsreeves.921@gmail.com>
2022-01-25 03:01:28 -0500

    Object requirements for choices

--- NoNameGame/Assets/Scripts/PlayerMovement.cs
+++ NoNameGame/Assets/Scripts/PlayerMovement.cs
@@ -101,10 +101,10 @@ public class PlayerMovement : MonoBehaviour
             //}
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        //if (Input.GetButtonDown("Jump") && isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        //}
 
         velocity.y += gravity * Time.deltaTime;
 
